71e39003afdaf52003826f25129a995ac83f1722
ramadhanafif <31378187+ramadhanafif@users.noreply.github.com>
2019-12-08 19:31:11 +0700

    Update stm32_rtos.ino

--- stm32_rtos/stm32_rtos.ino
+++ stm32_rtos/stm32_rtos.ino
@@ -40,12 +40,4 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(PA5, HIGH);
-  digitalWrite(PB1, LOW);
-
-  delay(100);
-  digitalWrite(PB1, HIGH);
-  digitalWrite(PA5, LOW);
-  delay(100);
 }
