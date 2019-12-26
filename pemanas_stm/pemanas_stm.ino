4793028482be57ef4994d60362628633aa1cacf6
ramadhanafif <31378187+ramadhanafif@users.noreply.github.com>
2019-12-26 19:33:16 +0700

    Pemanas v2 saran pa egi

--- pemanas_stm/pemanas_stm.ino
+++ pemanas_stm/pemanas_stm.ino
@@ -1,4 +1,4 @@
-/* KODE PENGENDALI PEMANAS
+/* KODE PENGENDALI PEMANAS v2
    BOARD: BLACKPILL
 */
 
@@ -7,58 +7,43 @@
 //Pin Definition
 #define SW PA0
 #define TEMP_SENSOR PA7
-
-//PID constants
-#define KP 10
-#define KI 0.011
-
-unsigned long currentTime, previousTime;
-double elapsedTime;
-double error;
-double input, output, setPoint;
-double cumError;
-double dutyCycle;
+#define BA 2.5
+#define BB 2.5
 
 void TaskCompute(void* v) {
-  setPoint = 80;                          //set point at zero degrees
+  double setPoint = 80;                          //set point at zero degrees
+  double input = 0;
+
+  uint8_t upcount = 0,
+          downcount = 0;
   Serial.begin(115200);
+  pinMode(SW, OUTPUT);
   pinMode(TEMP_SENSOR, INPUT_ANALOG);
   for (;;) {
     input = analogRead(TEMP_SENSOR);                //read from rotary encoder connected to A0
-    input = input / 12.409 + 5;
+    input = input / 12.409;
 
-    output = computePID(input);
-
-    dutyCycle = (double) output / 500.0;
-    if (dutyCycle > 1.0)
-      dutyCycle = 1;
-    else if (dutyCycle < 0.0)
-      dutyCycle = 0;
-
-    Serial.print(input); Serial.print(';');
-    Serial.print(output); Serial.print(';');
-    Serial.println(dutyCycle * 1);
-    vTaskDelay(1000);
-  }
-}
-
-void TaskSwitch(void* v) {
-  pinMode(SW, OUTPUT);
-  for (;;) {
-    digitalWrite(SW, HIGH);
-    vTaskDelay(200 * dutyCycle);
-    digitalWrite(SW, LOW);
-    vTaskDelay(200 * (1 - dutyCycle));
+    if (setPoint + BA >= input) {
+      upcount++; //mencegah lonjakan acak
+      if (upcount > 10) {
+        digitalWrite(SW, HIGH);
+        upcount = 0;
+        Serial.println(">>> PEMANAS ON");
+      }
+    }
+    else if (setPoint - BB <= input) {
+      downcount++;//mencegah lonjakan acak
+      if (downcount > 0) {
+        digitalWrite(SW, HIGH);
+        downcount = 0;
+        Serial.println(">>> PEMANAS OFF");
+      }
+    }
+    vTaskDelay(10);
   }
 }
 
 void setup() {
-  xTaskCreate(TaskSwitch,
-              "Task1",
-              configMINIMAL_STACK_SIZE,
-              NULL,
-              tskIDLE_PRIORITY + 1,
-              NULL);
   xTaskCreate(TaskCompute,
               "Task2",
               configMINIMAL_STACK_SIZE,
@@ -69,19 +54,4 @@ void setup() {
 }
 
 void loop() {
-}
-
-
-double computePID(double inp) {
-  currentTime = millis() / 1000;              //get current time
-  elapsedTime = (double)(currentTime - previousTime);        //compute time elapsed from previous computation
-
-  error = setPoint - inp;                                // determine error
-  cumError += error * elapsedTime;                // compute integral
-
-  double out = KP * error + KI * cumError;// + kd * rateError;          //PID output
-
-  previousTime = currentTime;                        //remember current time
-
-  return out;                                        //have function return the PID output
 }
