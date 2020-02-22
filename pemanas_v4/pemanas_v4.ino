974ec08d8100608b15b930a33da3af68d9f8e869
ramadhanafif <31378187+ramadhanafif@users.noreply.github.com>
2020-02-22 17:04:02 +0700

    versi baru dengan PWM

--- pemanas_v4/pemanas_v4.ino
+++ pemanas_v4/pemanas_v4.ino
@@ -1,26 +1,30 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-#define ONE_WIRE_BUS2 4
-OneWire oneWire2(ONE_WIRE_BUS2);
-DallasTemperature sensor2(&oneWire2);
+#define TEMP_SENSOR_PIN 4
+OneWire oneWire(TEMP_SENSOR_PIN);
+DallasTemperature sensor(&oneWire);
 
 //Pin Definition
-#define SW 2
-#define BA -0.7
+#define SSR_PIN 2 
 #define BB 1
 
-#define TIME_ON_PMNS 40
-#define SET_POINT_DEBUG 90
+#define PMNS_WAIT_TIME 40
+#define PMNS_ON_TIME 40
 
-#define PERIOD_PWM 400
+#define PMNS_SET_POINT_DEBUG 90
+#define PMNS_PERIOD_PWM 400
+
+#define PMNS_STATE_START 0
+#define PMNS_STATE_STEADY 1
+
 
 TaskHandle_t xPWMHandle = NULL;
-unsigned int state = 0;
-unsigned int ssr = 2;
-double input = 0;
-unsigned int counter = 0;
-double dutyCycle = 0;
+
+unsigned int PMNS_pemanas_state = 0;
+unsigned int PMNS_flag_pemanas_awal_done = 0;
+
+double TempRead = 0;
 
 double get_temp(DallasTemperature sensor) {
   sensor.requestTemperatures();
@@ -33,82 +37,109 @@ void TaskPrint(void* v) {
   xLastWakeTime = xTaskGetTickCount();
   for (;;) {
     vTaskDelayUntil( &xLastWakeTime, 1000);
-    Serial.print(input); Serial.print(" ");
-    Serial.print(state); Serial.print(" ");
-    Serial.print(ssr); Serial.print(" ");
+    Serial.print(TempRead); Serial.print(" ");
+    Serial.print(PMNS_pemanas_state); Serial.print(" ");
     Serial.println();
   }
 }
 
-void TaskPMNS(void* v) {
-  double setPoint = SET_POINT;                          //set point at zero degrees
-  unsigned int prev_input;
-  unsigned int time_on = 0;
-
+void taskPMNS_MAIN(void* v) {
+  double setPoint = PMNS_SET_POINT_DEBUG;
   double prevtime = millis();
   double cumerror = 0;
-
+  
+  unsigned int prev_TempRead;
   unsigned int temp_counter = 0;
+  unsigned int PMNS_ssr = 2;
+  unsigned int PMNS_counter = 0;
 
-  pinMode(SW, OUTPUT);
-  sensor2.begin();
+  const int pwm_freq = 2;
+  const int pwm_ledChannel = 2;
+  const int pwm_resolution = 15;
+
+  unsigned int pwm_attach = 0;
+
+  ledcSetup(pwm_ledChannel, pwm_freq, pwm_resolution);
+
+  float dutyCycle = 0;
+
+  sensor.begin();
+
   TickType_t xLastWakeTime = xTaskGetTickCount();
-
   for (;;) {
     vTaskDelayUntil( &xLastWakeTime, 1000);
-    input = get_temp(sensor2);
-    switch (state) {
-      case 0: //PID
+    TempRead = get_temp(sensor);
+    switch (PMNS_pemanas_state) {
+      case PMNS_STATE_START: //PID
         {
-          double output = computePID(input, setPoint, &prevtime, &cumerror);
+          //PID calculation
+          double output = PMNS_computePID(TempRead, setPoint, &prevtime, &cumerror);
 
+          //Normalization PID to dutyCycle (0 - 4095)
           dutyCycle = output / 500.0;
           if (dutyCycle > 1.0)
             dutyCycle = 1;
           else if (dutyCycle < 0.0)
             dutyCycle = 0;
+          dutyCycle *= 4095;
 
-          if (abs(input - setPoint) <= 2.5) {
-            temp_counter++;
+          //PWM pin initialization
+          if (!pwm_attach)
+          {
+            ledcAttachPin(SSR_PIN, pwm_ledChannel);
+            pwm_attach = 1;
           }
 
+          //PWM update dutyCycle 
+          ledcWrite(pwm_ledChannel,dutyCycle);
+
+          //Check for temperature steadiness
+          if (abs(TempRead - setPoint) <= 2.5) {
+            temp_counter++;
+          }
           if (temp_counter >= 60) {
-            state++;
+            PMNS_flag_pemanas_awal_done = 1;
           }
           break;
         }
-      case 1: //On Off
+      case PMNS_STATE_STEADY: //On Off
         {
-          vTaskSuspend(xPWMHandle); //delete PWM.
-          switch (ssr)
+          //Stop PWM
+          if (pwm_attach){
+            ledcDetachPin(SSR_PIN);
+            pwm_attach = 0;
+            pinMode(SSR_PIN, OUTPUT);
+          }
+
+          switch (PMNS_ssr)
           {
             case 0: //nunggu sampe turun dari kelebihan suhu
-              digitalWrite(SW, ssr);
-              if (input <= setPoint - BB) {
-                if (abs(prev_input - input) < 2) {
-                  ssr = 1; //change state
-                  counter = TIME_ON;
+              digitalWrite(SSR_PIN, 0);
+              if (TempRead <= setPoint - BB) {
+                if (abs(prev_TempRead - TempRead) < 2) {
+                  PMNS_ssr = 1; //change state
+                  PMNS_counter = PMNS_ON_TIME;
                 }
               }
               break;
-            case 1: //on TIME_ON s
-              digitalWrite(SW, ssr);
-              counter--;
-              if (counter == 0)
+            case 1: //decrement from PMNS_ON_TIME until 0
+              digitalWrite(SSR_PIN, PMNS_ssr);
+              PMNS_counter--;
+              if (PMNS_counter == 0)
               {
-                ssr = 2;
+                PMNS_ssr = 2;
               }
               break;
             case 2: //nunggu TIME_ON s
-              digitalWrite(SW, 0);
-              counter++;
-              if (counter == TIME_ON)
+              digitalWrite(SSR_PIN, 0);
+              PMNS_counter++;
+              if (PMNS_counter == PMNS_WAIT_TIME)
               {
-                ssr = 0;
+                PMNS_ssr = 0;
               }
               break;
           }
-          prev_input = input;
+          prev_TempRead = TempRead;
 
           break; //break state 1
         }
@@ -116,20 +147,11 @@ void TaskPMNS(void* v) {
   }
 }
 
-void TaskPWMPMNS(void* v) {
-  pinMode(SW, OUTPUT);
-  for (;;) {
-    digitalWrite(SW, HIGH);
-    vTaskDelay(dutyCycle * PERIOD);
-    digitalWrite(SW, LOW);
-    vTaskDelay((1 - dutyCycle)*PERIOD);
-  }
-}
 
 void setup() {
 
   xTaskCreate(
-    TaskPMNS,
+    taskPMNS_MAIN,
     "Task2",
     1024,
     NULL,
@@ -145,21 +167,12 @@ void setup() {
     tskIDLE_PRIORITY + 3,
     NULL
   );
-
-  xTaskCreate(
-    TaskPWMPMNS,
-    "Task2",
-    1024,
-    NULL,
-    tskIDLE_PRIORITY + 1,
-    &xPWMHandle
-  );
 }
 
 void loop() {
 }
 
-double computePID(double inp, unsigned int setPoint, double* previousTime, double* cumError) {
+double PMNS_computePID(double inp, unsigned int setPoint, double* previousTime, double* cumError) {
   double error = 0;
   double elapsedTime = 0;
   double currentTime;
@@ -167,15 +180,15 @@ double computePID(double inp, unsigned int setPoint, double* previousTime, doubl
   double kp = 8; //10
   double ki = 0.005; //0.011
 
-  currentTime = millis() / 1000;              //get current time
+  currentTime = millis() / 1000;                      //get current time
   elapsedTime = (currentTime - *previousTime);        //compute time elapsed from previous computation
 
-  error = setPoint - inp;                                // determine error
-  *cumError += error * elapsedTime;                // compute integral
+  error = setPoint - inp;                             // determine error
+  *cumError += error * elapsedTime;                   // compute integral
 
-  double out = kp * error + ki * *cumError;// + kd * rateError;          //PID output
+  double out = kp * error + ki * *cumError;           // + kd * rateError;          //PID output
 
   *previousTime = currentTime;                        //remember current time
 
-  return out;                                        //have function return the PID output
+  return out;                                         //have function return the PID output
 }
