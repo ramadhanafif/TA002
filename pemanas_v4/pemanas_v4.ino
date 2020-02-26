1aa7402bbe9895c0a33cf077e1aa4cde6aa2410d
ramadhanafif <31378187+ramadhanafif@users.noreply.github.com>
2020-02-26 15:46:29 +0700

    Update pemanas_v4.ino

--- pemanas_v4/pemanas_v4.ino
+++ pemanas_v4/pemanas_v4.ino
@@ -1,20 +1,19 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include "MedianFilterLib.h"
 
 //Definition
-#define TEMP_SENSOR_PIN 26
-#define SSR_PIN 23 
+#define TEMP_SENSOR_PIN 23
+#define SSR_PIN 26
 
 #define BB 1
 
 #define PMNS_WAIT_TIME 40
-#define PMNS_ON_TIME 40
-#define PMNS_SET_POINT_DEBUG 75
+#define PMNS_ON_TIME 30
+#define PMNS_SET_POINT_DEBUG 90
 
 #define PMNS_STATE_START 0
 #define PMNS_STATE_STEADY 1
-
-#define PRINT_DONG
 
 OneWire oneWire(TEMP_SENSOR_PIN);
 DallasTemperature sensor(&oneWire);
@@ -22,14 +21,17 @@ DallasTemperature sensor(&oneWire);
 unsigned int PMNS_pemanas_state = 0;
 unsigned int PMNS_flag_pemanas_awal_done = 0;
 double TempRead = 0;
-float dutyCycle = 0;
 
 double get_temp(DallasTemperature sensor) {
-  sensor.requestTemperatures();
-  return sensor.getTempCByIndex(0);
-}
+  MedianFilter<double> medianFilter(3);
+  for (int x = 0 ; x < 2 ; x++) {
+    sensor.requestTemperatures();
+    medianFilter.AddValue(sensor.getTempCByIndex(0));
+  }
 
-#ifdef PRINT_DONG
+  sensor.requestTemperatures();
+  return medianFilter.AddValue(sensor.getTempCByIndex(0));
+}
 void TaskPrint(void* v) {
   Serial.begin(115200);
   TickType_t xLastWakeTime;
@@ -38,35 +40,33 @@ void TaskPrint(void* v) {
     vTaskDelayUntil( &xLastWakeTime, 1000);
     Serial.print(TempRead); Serial.print(" ");
     Serial.print(PMNS_pemanas_state); Serial.print(" ");
-    Serial.print(dutyCycle); Serial.print(" ");
     Serial.println();
   }
 }
-#endif
-
 
 void taskPMNS_MAIN(void* v) {
   double setPoint = PMNS_SET_POINT_DEBUG;
   double prevtime = millis();
   double cumerror = 0;
-  
-  unsigned int prev_TempRead;
+
+  //  unsigned int prev_TempRead;
   unsigned int temp_counter = 0;
-  unsigned int PMNS_ssr = PMNS_STATE_START;
+  unsigned int PMNS_ssr = 2;
   unsigned int PMNS_counter = 0;
 
   const int pwm_freq = 2;
   const int pwm_ledChannel = 2;
-  const int pwm_resolution = 15;
+  const int pwm_resolution = 16;
 
   unsigned int pwm_attach = 0;
 
   ledcSetup(pwm_ledChannel, pwm_freq, pwm_resolution);
 
-  // float dutyCycle = 0;
+  float dutyCycle = 0;
 
+  pinMode(SSR_PIN, OUTPUT);
   sensor.begin();
-  //vTaskSuspend(NULL);
+
   TickType_t xLastWakeTime = xTaskGetTickCount();
   for (;;) {
     vTaskDelayUntil( &xLastWakeTime, 1000);
@@ -83,7 +83,7 @@ void taskPMNS_MAIN(void* v) {
             dutyCycle = 1;
           else if (dutyCycle < 0.0)
             dutyCycle = 0;
-          dutyCycle *= 4095;
+          dutyCycle *= 0xFFFF;
 
           //PWM pin initialization
           if (!pwm_attach)
@@ -92,8 +92,8 @@ void taskPMNS_MAIN(void* v) {
             pwm_attach = 1;
           }
 
-          //PWM update dutyCycle 
-          ledcWrite(pwm_ledChannel,dutyCycle);
+          //PWM update dutyCycle
+          ledcWrite(pwm_ledChannel, dutyCycle);
 
           //Check for temperature steadiness
           if (abs(TempRead - setPoint) <= 2.5) {
@@ -106,34 +106,31 @@ void taskPMNS_MAIN(void* v) {
         }
       case PMNS_STATE_STEADY: //On Off
         {
-          //Stop PWM
-          if (pwm_attach){
+          //Stop PWM, switch to bang-bang
+          if (pwm_attach) {
             ledcDetachPin(SSR_PIN);
             pwm_attach = 0;
-            pinMode(SSR_PIN, OUTPUT);
           }
 
           switch (PMNS_ssr)
           {
             case 0: //nunggu sampe turun dari kelebihan suhu
-              digitalWrite(SSR_PIN, 0);
+              digitalWrite(SSR_PIN, LOW);
               if (TempRead <= setPoint - BB) {
-                if (abs(prev_TempRead - TempRead) < 2) {
-                  PMNS_ssr = 1; //change state
-                  PMNS_counter = PMNS_ON_TIME;
-                }
+                PMNS_ssr = 1; //change state
+                PMNS_counter = PMNS_ON_TIME;
               }
               break;
             case 1: //decrement from PMNS_ON_TIME until 0
-              digitalWrite(SSR_PIN, PMNS_ssr);
+              digitalWrite(SSR_PIN, HIGH);
               PMNS_counter--;
               if (PMNS_counter == 0)
               {
                 PMNS_ssr = 2;
               }
               break;
-            case 2: //nunggu TIME_ON s
-              digitalWrite(SSR_PIN, 0);
+            case 2: //nunggu selama WAIT TIME
+              digitalWrite(SSR_PIN, LOW);
               PMNS_counter++;
               if (PMNS_counter == PMNS_WAIT_TIME)
               {
@@ -141,8 +138,6 @@ void taskPMNS_MAIN(void* v) {
               }
               break;
           }
-          prev_TempRead = TempRead;
-
           break; //break state 1
         }
     }
@@ -155,7 +150,7 @@ void setup() {
   xTaskCreate(
     taskPMNS_MAIN,
     "Task2",
-    1024,
+    1 << 11,
     NULL,
     tskIDLE_PRIORITY + 2,
     NULL
